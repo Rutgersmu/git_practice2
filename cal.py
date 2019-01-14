b627519322b6db8e1ddbe80f549d61d998a5fa5b
Rutgersmu <softstep1221@gmail.com>
2019-01-14 14:31:05 +0900

    div 함수 구현

--- cal.py
+++ cal.py
@@ -8,3 +8,12 @@ def two_minus(a, b):
 
 def three_minus(a, b, c):
     return a - b - c
+
+def two_multy(a, b):
+    return a * b
+
+def two_div(a, b):
+    return a / b
+
+def three_div(a, b, c)
+    return a / b / c
